b16148bad368a5d1d2c592aaf8344dc3ca741b3f
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-01-08 16:35:36 +0530

    Create program to check positive or negative.c

---
+++ program to check positive or negative.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+double number;
+printf("enter a number");
+scanf("%ld",&d);
+if(1<=N<=100000)
+{
+if(N=0)
+Printf("you entered zero");
+else
+printf("you entered a negative number");
+}
+else
+printf("youn entered a positive number");
+return 0;
+}
