e8a3ceb26ab1ae1ba5948ba52c958057030999b0
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-01-20 14:39:22 +0530

    Update armn.c

--- armn.c
+++ armn.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+ 
+void main()
+{
+    int n, sum = 0, rem = 0, cube = 0, temp;
+ 
+    
+    scanf("%d", &n);
+    temp = n;
+    while (n != 0)
+    {
+        rem = n % 10;
+        cube = pow(rem, 3);
+        sum = sum + cube;
+        n= n / 10;
+    }
+    if (sum == temp)
+        printf ("yes");
+    else
+        printf ("no");
+}
