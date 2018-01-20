6c7a8224eb21d377f10697661c3a15a397d8a33a
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-01-20 14:27:46 +0530

    Update arm2.c

--- arm2.c
+++ arm2.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    int low, high, i, temp1, temp2, remainder, n = 0, result = 0;
+    int low, high, i, temp1, temp2, r, n = 0, result = 0;
 
     printf("Enter two numbers(intervals): ");
     scanf("%d %d", &low, &high);
@@ -14,27 +13,27 @@ int main()
         temp2 = i;
         temp1 = i;
 
-        // number of digits calculation
+    
         while (temp1 != 0)
         {
             temp1 /= 10;
             ++n;
         }
 
-        // result contains sum of nth power of its digits
+       
         while (temp2 != 0)
         {
-            remainder = temp2 % 10;
-            result += pow(remainder, n);
+            r = temp2 % 10;
+            result += pow(r, n);
             temp2 /= 10;
         }
 
-        // checks if number i is equal to the sum of nth power of its digits
+       
         if (result == i) {
             printf("%d ", i);
         }
 
-        // resetting the values to check Armstrong number for next iteration
+     
         n = 0;
         result = 0;
 
