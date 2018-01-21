15fa94e2d9cfaf23160454613a7e3005e57fea45
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-01-21 10:06:11 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int n,h,m;
+    scanf("%d",&n);
+    if(n<60)
+    {
+    	printf("0\t%d",n);
+    }
+    if(n>60)
+    {
+    h=n/60;
+    m=n%60;
+    printf("%d\t%d",h,m);
+    }
+}
