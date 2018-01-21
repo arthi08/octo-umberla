750aa9df85df09bcbd3add29b8ca23d3a6e557b4
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-01-21 06:45:32 +0000

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+char ch;
+   unsigned int long linecount, wordcount, charcount;
+   int u;
+   linecount=0;
+   wordcount=0;
+   charcount=0;
+ while((ch=getc(stdin))!=EOF){
+     if (ch !='\n') {++charcount;}
+       if (ch==' ' || ch=='\n') {++wordcount;}
+       if (ch=='\n') {++linecount;}
+
+     }
+   if(charcount>0)
+   {
+      ++wordcount;
+      ++linecount;
+   }
+  printf( "%lu %lu %lu\n", charcount, wordcount, linecount );
+   }
