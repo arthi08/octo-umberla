9e1b4e15e4f3ff8a87597262a8985b5b2a8df643
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-02-05 16:07:38 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+long c=1;
+printf("Enter the base number");
+scanf("%d",&a);
+printf("Enter the exponent number");
+scanf("%d",&b);
+while(b!=0)
+{
+c*=a;
+b--;
+}
+printf("The result is :%lld",c);
+}
