3f8127456aa080d7879d691d0575bdd251f1402c
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-02-05 15:55:46 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,15 @@
+ude<stdio.h>
+void main()
+{
+int a[100],i,n,c,d;
+printf("Enter the  number");
+scanf("%d",&n);
+printf("Enter the numbers");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+c+=a[i];
+}
+d=c/n;
+printf("The result is :%d",d);
+}
