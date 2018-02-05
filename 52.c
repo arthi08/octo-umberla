193ef2d268eb6b28f368d05df011d02ef63fde71
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-02-05 16:37:38 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case1:
+printf("one");
+break;
+case2:
+printf("two");
+break;
+case3:
+printf("three");
+break;
+case4:
+printf("four");
+break;
+default:
+printf("number not matched");
+}
+}
+
