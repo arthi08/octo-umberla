9790a2366b714fb6ed4b07fbfa0757e6d5eef9ea
arthi08 <31499163+arthi08@users.noreply.github.com>
2018-02-05 15:40:32 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+void main() {
+  char a[1000],b[1000];
+  int i,j,count1=0,count2=0;
+  scanf("%s",&a);
+  scanf("%s",&b);
+  printf("%s%s",a,b);   //direct way
+  while(a[i]!='\0')
+  i++;
+  while(b[j]!='\0')
+  {
+    a[i]=b[j];
+    j++;
+    i++;
+  }
+  a[i]='\0';
+  printf("%s",a);
+  }
